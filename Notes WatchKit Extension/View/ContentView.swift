d3e06461157eb515dea7a54c380556e9e19f3d8a
Nerijus Šurkys <46633513+surkeela@users.noreply.github.com>
2022-05-16 15:35:24 +0300

    WP
    add input interface

--- Notes WatchKit Extension/View/ContentView.swift
+++ Notes WatchKit Extension/View/ContentView.swift
@@ -13,6 +13,12 @@ struct ContentView: View {
     @State private var notes: [Note] = [Note]()
     @State private var text: String = ""
     
+    // MARK: - FUNCTION
+    
+    func save() {
+        dump(notes)
+    }
+    
     // MARK: - BODY
     
     var body: some View {
@@ -22,10 +28,19 @@ struct ContentView: View {
                 
                 Button {
                     // 1. Only run the button's action when the text field is not empty
+                    guard text.isEmpty == false else { return }
+                    
                     // 2. Create a new note item and initialize it with the text value
+                    let note = Note(id: UUID(), text: text)
+                    
                     // 3. Add the note item to the notes array (append)
+                    notes.append(note)
+                    
                     // 4. Make the text field empty
+                    text = ""
+                    
                     // 5. Save the notes (function)
+                    save()
                 } label: {
                     Image(systemName: "plus.circle")
                         .font(.system(size: 42, weight: .semibold))
@@ -37,6 +52,8 @@ struct ContentView: View {
             } //: HSTACK
             
             Spacer()
+            
+            Text("\(notes.count)")
         } //: VSTACK
         .navigationTitle("Notes")
     }
