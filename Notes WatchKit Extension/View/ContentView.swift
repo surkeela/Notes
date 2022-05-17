162056e67d69e75e61ff4ee14a83f5d8f99ff46c
Nerijus Šurkys <46633513+surkeela@users.noreply.github.com>
2022-05-17 14:55:59 +0300

    WP
    create a detail view for note

--- Notes WatchKit Extension/View/ContentView.swift
+++ Notes WatchKit Extension/View/ContentView.swift
@@ -98,14 +98,16 @@ struct ContentView: View {
             if notes.count >= 1 {
                 List {
                     ForEach(0..<notes.count, id: \.self) { i in
-                        HStack {
-                            Capsule()
-                                .frame(width: 4)
-                                .foregroundColor(.accentColor)
-                            Text(notes[i].text)
-                                .lineLimit(1)
-                                .padding(.leading, 5)
-                        } //: HSTACK
+                        NavigationLink(destination: DetaiView(note: notes[i], count: notes.count, index: i)) {
+                            HStack {
+                                Capsule()
+                                    .frame(width: 4)
+                                    .foregroundColor(.accentColor)
+                                Text(notes[i].text)
+                                    .lineLimit(1)
+                                    .padding(.leading, 5)
+                            }
+                        } //: NAVIGATION
                     } //: LOOP
                     .onDelete(perform: delete)
                 }
---
+++ Notes WatchKit Extension/View/DetaiView.swift
@@ -0,0 +1,73 @@
+//
+//  DetaiView.swift
+//  Notes WatchKit Extension
+//
+//  Created by Nerijus Surkys on 2022-05-17.
+//
+
+import SwiftUI
+
+struct DetaiView: View {
+    // MARK: - PROPERTY
+    
+    let note: Note
+    let count: Int
+    let index: Int
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 3) {
+            // HEADER
+            HStack {
+                Capsule()
+                    .frame(height: 1)
+                
+                Image(systemName: "note.text")
+                
+                Capsule()
+                    .frame(height: 1)
+            } //: HSTACK
+            .foregroundColor(.accentColor)
+            
+            // CONTENT
+            Spacer()
+            
+            ScrollView(.vertical) {
+                Text(note.text)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+            }
+            
+            Spacer()
+            
+            // FOOTER
+            HStack(alignment: .center) {
+                Image(systemName: "gear")
+                    .imageScale(.large)
+                
+                Spacer()
+                
+                Text("\(count) / \(index + 1)")
+                
+                Spacer()
+                
+                Image(systemName: "info.circle")
+                    .imageScale(.large)
+            } //: HSTACK
+            .foregroundColor(.secondary)
+        } //: VSTACK
+        .padding(3)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct DetaiView_Previews: PreviewProvider {
+    static var sampleData: Note = Note(id: UUID(), text: "Hello, World!")
+    
+    static var previews: some View {
+        DetaiView(note: sampleData, count: 5, index: 1)
+    }
+}
